93674e611062fccc7d731e60f5d3cb4aa2dbbccd
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2024-11-26 11:09:15 +0700

    edit.php

--- app/views/user/edit.php
+++ app/views/user/edit.php
@@ -3,19 +3,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Edit User</title>
+    <title>Edit Pendaftaran Peserta</title>
 </head>
 <body>
-    <h2>Edit User</h2>
-    <form action="/user/update/<?php echo $user['id']; ?>" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" value="<?php echo $user['name']; ?>" required>
+    <h2>Edit Pendaftaran Peserta</h2>
+    <form action="/datapeserta/update/<?php echo $peserta['id']; ?>" method="POST">
+        <pre>
+        <label for="peserta">Nama Peserta:</label>
+        <input type="text" id="peserta" name="peserta" value="<?php echo $peserta['peserta']; ?>" required>
         <br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>" required>
+        <label for="kursus">Jenis Kursus:</label>
+        <input type="text" id="kursus" name="kursus" value="<?php echo $peserta['kursus']; ?>" required>
+        <br>
+        <label for="tanggal_daftar">Tanggal Daftar:</label>
+        <input type="date" id="tanggal_daftar" name="tanggal_daftar" value="<?php echo $peserta['tanggal_daftar']; ?>" required>
         <br>
         <button type="submit">Update</button>
+        </pre>
     </form>
-    <a href="/user/index">Back to List</a>
+    <a href="/datapeserta/index">Back to List</a>
 </body>
 </html>
