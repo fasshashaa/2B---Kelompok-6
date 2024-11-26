d00936393005791d5fa318ccd2248678393b9a7a
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2024-11-26 14:55:06 +0700

    edit.php

--- app/views/user/edit.php
+++ app/views/user/edit.php
@@ -7,10 +7,11 @@
 </head>
 <body>
     <h2>Edit Pendaftaran Peserta</h2>
-    <form action="/datapeserta/update/<?php echo $peserta['id']; ?>" method="POST">
+    <form action="/user/update/<?php echo $peserta['id']; ?>" method="POST">
         <pre>
         <label for="peserta">Nama Peserta:</label>
-        <input type="text" id="peserta" name="peserta" value="<?php echo $peserta['peserta']; ?>" required>
+        <input type="text" id="name" name="name" value="<?php echo $peserta['peserta']; ?>" required>
+        <input type="hidden" id="id" name="id" value="<?php echo $peserta['id']; ?>" required>
         <br>
         <label for="kursus">Jenis Kursus:</label>
         <input type="text" id="kursus" name="kursus" value="<?php echo $peserta['kursus']; ?>" required>
@@ -19,8 +20,8 @@
         <input type="date" id="tanggal_daftar" name="tanggal_daftar" value="<?php echo $peserta['tanggal_daftar']; ?>" required>
         <br>
         <button type="submit">Update</button>
-        </pre>
+</pre>
     </form>
-    <a href="/datapeserta/index">Back to List</a>
+    <a href="/user/index">Back to List</a>
 </body>
 </html>
