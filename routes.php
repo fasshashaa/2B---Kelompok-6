63d38a86725248553b6e67a8629e43b0f79d1b66
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2024-11-26 14:56:35 +0700

    routes.php

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
 
 require_once 'app/controllers/EnrollmentController.php';
 
-$controller = new Controller(); // Ganti UserController dengan DataPesertaController
+$controller = new EnrollmentController();
 $url = $_SERVER['REQUEST_URI'];
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
@@ -14,14 +14,14 @@ if ($url == '/user/index' || $url == '/') {
 } elseif ($url == '/user/store' && $requestMethod == 'POST') {
     $controller->store();
 } elseif (preg_match('/\/user\/edit\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $pesertaId = $matches[1];
-    $controller->edit($pesertaId);
+    $userId = $matches[1];
+    $controller->edit($userId);
 } elseif (preg_match('/\/user\/update\/(\d+)/', $url, $matches) && $requestMethod == 'POST') {
-    $pesertaId = $matches[1];
-    $controller->update($pesertaId, $_POST);
+    $userId = $matches[1];
+    $controller->update($userId, $_POST);
 } elseif (preg_match('/\/user\/delete\/(\d+)/', $url, $matches) && $requestMethod == 'GET') {
-    $pesertaId = $matches[1];
-    $controller->delete($pesertaId);
+    $userId = $matches[1];
+    $controller->delete($userId);
 } else {
     http_response_code(404);
     echo "404 Not Found";
