338fb5b6e397d92e9d9eeb0118f17b6c61a3bfb1
anafebrisalusi <165301550+anafebrisalusi@users.noreply.github.com>
2024-11-26 11:08:42 +0700

    create.php

--- app/views/user/create.php
+++ app/views/user/create.php
@@ -1,9 +1,13 @@
 <!-- app/views/user/create.php -->
-<h2>Tambah Pengguna Baru</h2>
+<h2>Pendaftaran Peserta</h2>
 <form action="/user/store" method="POST">
-    <label for="name">Nama:</label>
-    <input type="text" name="name" id="name" required>
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" required>
+    <pre>
+    <label for="peserta">Nama Peserta:</label>
+    <input type="text" name="peserta" id="peserta" required><br>
+    <label for="kursus">Jenis Kursus:</label>
+    <input type="text" name="kursus" id="kursus" required><br>
+    <label for="tanggal_daftar">Tanggal Daftar:</label>
+    <input type="date" name="tanggal_daftar" id="tanggal_daftar" required><br>
     <button type="submit">Simpan</button>
+    </pre>
 </form>
